b2ce6c2c73672399a7dd1edb4c843e5c2d6e8e94
purelind <kevinsocial@outlook.com>
2017-07-17 22:21:14 +0800

    single Linked List reverse with iterattion

--- basic-data-structure/list_node.py
+++ basic-data-structure/list_node.py
@@ -7,3 +7,18 @@ class ListNode:
         return str(self.val)
 
 
+def reverse_iter(head):
+    """
+
+    :param head: ListNode, head node on the single way ListNode
+    :return: ListNode, the new head node when ListNode reversed
+    """
+    current_index = None
+    while head:
+        temp_node = head.next
+        head.next = current_index
+        current_index = head
+        head = temp_node
+    return current_index
+
+
