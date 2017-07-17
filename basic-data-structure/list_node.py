b46fb2f0188bafe9a8695f3cfa2c456138a16848
purelind <kevinsocial@outlook.com>
2017-07-17 22:22:31 +0800

    single Linked List reverse with recursion

--- basic-data-structure/list_node.py
+++ basic-data-structure/list_node.py
@@ -22,3 +22,16 @@ def reverse_iter(head):
     return current_index
 
 
+def reverse_recur(head):
+    """
+
+    :param head: ListNode, head node on the single way ListNode
+    :return: ListNode, the new head node when ListNode reversed
+    """
+    if head is None or head.next is None:
+        return head
+    next_node = head.next
+    new_head = reverse_recur(next_node)
+    next_node.next = head
+    head.next = None
+    return new_head
