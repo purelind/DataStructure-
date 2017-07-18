e3976b2b1065ae1cc0872e49069f8964bdbdf5b9
purelind <kevinsocial@outlook.com>
2017-07-18 21:20:31 +0800

    double list node reversed by iteration

--- basic-data-structure/double_list_node.py
+++ basic-data-structure/double_list_node.py
@@ -2,3 +2,20 @@ class DListNode:
     def __init__(self, val):
         self.val = val
         self.prev = self.next = None
+
+
+def reverse_iter(head):
+    """
+
+    :param head: DListNode, head double list node
+    :return: DListNode, the new head double list node when reversed
+    """
+    current = None
+    while head:
+        current = head
+        head = current.next
+        current.next = current.prev
+        current.prev = head
+    return current
+
+
