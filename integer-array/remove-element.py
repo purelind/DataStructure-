0d74821d3ebd3afd65cc3efbeedec6759c7dce94
purelind <kevinsocial@outlook.com>
2017-08-09 16:29:41 +0800

    Add to List 27. Remove Element, left++

---
+++ integer-array/remove-element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        left = 0
+        for num in nums:
+            if num != val:
+                nums[left] = num
+                left += 1
+        return left
+
+solve = Solution()
+print(solve.removeElement([3, 2, 2, 3], 3))
